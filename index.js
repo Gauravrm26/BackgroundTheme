6d5a8fb28686f1f5f77338f1e9fa0c30709bc517
Gaurav RM <85059991+Gauravrm26@users.noreply.github.com>
2021-07-12 23:12:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+$("#greyButton").click(function(){
+    $("body").css("background-color", "rgb(158, 155, 155)")
+}); 
+$("#whiteButton").click(function(){
+    $("body").css("background-color", "white")
+}); 
+$("#blueButton").click(function(){
+    $("body").css("background-color", "rgb(86, 86, 231)")
+}); 
+$("#yellowButton").click(function(){
+    $("body").css("background-color", "rgb(235, 235, 95)")
+}); 
+
