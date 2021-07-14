75e2f6b04063b01a7a9bae4adb7f1ead39088f33
Gaurav RM <85059991+Gauravrm26@users.noreply.github.com>
2021-07-14 17:52:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ $("#yellowButton").click(function(){
     $("body").css("background-color", "rgb(235, 235, 95)")
 }); 
 
+$("#greyButton").click(function(){
+    $("body").css("color", "black")
+}); 
+$("#whiteButton").click(function(){
+    $("body").css("color", "black")
+}); 
+$("#blueButton").click(function(){
+    $("body").css("color", "white")
+}); 
+$("#yellowButton").click(function(){
+    $("body").css("color", "white")
+});
